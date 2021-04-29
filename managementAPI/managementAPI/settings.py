5c2a4493e0bf66d968124f9bdd6b68d1e60240a3
kazuakihiga <higak926@gmail.com>
2021-04-29 17:10:26 +0900

    create local settings

--- managementAPI/managementAPI/settings.py
+++ managementAPI/managementAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+86v$hn6n66tp$br+t1t#chuqc74ilbozwf60s*vfj9+9o55=9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
